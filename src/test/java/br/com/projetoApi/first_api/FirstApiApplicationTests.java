cb53264c0d31828463b9b14e266e1e699751268f
ramosrodrigo <ramosrodrigo23@outlook.com>
2022-12-15 18:10:24 -0300

    adicionando o projeto inicial spring

---
+++ src/test/java/br/com/projetoApi/first_api/FirstApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetoApi.first_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
